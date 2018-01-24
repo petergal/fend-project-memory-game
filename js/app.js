7c9c1c287d706308302dc8dce3486da3a4ffcbec
Peter Gal <petergal@pgMBP2017.local>
2018-01-24 22:29:45 +0100

    refactor: Changes in JS comments

--- js/app.js
+++ js/app.js
@@ -32,12 +32,13 @@ init();
 
 /**
  * Initialize the game.
+ * Shuffle the cards and initialize the score panel.
  */
 function init() {
   let newCardList = createNewDeck();
   insertNewDeck(newCardList);
   initScorePanel();
-  /** Show game and hide result page */
+  // Show game and hide result page
   document.querySelector('header').classList.remove("hide");
   document.querySelector('.score-panel').classList.remove("hide");
   document.querySelector('.deck').classList.remove("hide");
@@ -45,12 +46,14 @@ function init() {
 }
 
 /**
- * Shuffle the cards ('LI' elements).
+ * Shuffle the cards.
  * @return {object} all card elements
  */
 function createNewDeck() {
+  // 'LI' elements represent the cards
   const cardList = shuffle([...document.getElementsByClassName('card')]);
   const newCardList = new Array();
+  // Hide the cards
   cardList.forEach(function(element) {
     element.classList.remove('match');
     element.classList.remove('open');
@@ -61,7 +64,7 @@ function createNewDeck() {
 }
 
 /**
- * Append card elements to parent element deck.
+ * Remove the last card deck and append the new shuffled deck.
  * @param  {object} newCardList
  */
 function insertNewDeck(newCardList) {
@@ -87,7 +90,8 @@ function removeCurrentDeck() {
 }
 
 /**
- * Initialize the score panel with move counter, star counter, star symbols and the timer.
+ * Initialize the score panel with move counter,
+ * star counter, star symbols and the timer.
  */
 function initScorePanel() {
   openCards = new Array(2);
@@ -128,7 +132,7 @@ function countTimer() {
 }
 
 /**
- * Format function as part of the timer function.
+ * Format the digits of the timer.
  * @param  {Number} digit
  * @return {string} formatted digit
  */
@@ -141,7 +145,8 @@ function format(digit) {
 }
 
 /**
- * Event listener definition and implementation of event handler function for card click events.
+ * Event listener definition and implementation of event handler function
+ * for card click events.
  */
 document.getElementsByClassName('deck')[0].addEventListener('click',
   function(event) {
@@ -150,37 +155,31 @@ document.getElementsByClassName('deck')[0].addEventListener('click',
      * @type {object}
      */
     let cardElement = event.target.closest('LI');
-    /** Validate a click event. */
+    // Validate a click event
     if ((cardElement.nodeName !== null) && (cardElement.nodeName === 'LI') &&
       (cardCounter < 2) && !(cardElement.classList.contains('open'))) {
-      /** Start timer at first card click. */
+      // Start timer at first card click
       if (intervalId === null) {
         document.getElementById('realtime').textContent = '00:00';
         intervalId = setInterval(countTimer, 1000);
       }
       cardCounter++;
+      // Show card
       cardElement.classList.add('open');
       cardElement.classList.add('show');
-      /** One card opened. */
+      // One card opened with this click
       if (cardCounter === 1) {
         openCards[0] = cardElement;
-        /** Two cards opened */
+        // Two cards opened with this click
       } else {
         openCards[1] = cardElement;
-        /** Two cards openend and match. */
+        // The cards match
         if (openCards[0].firstElementChild.className === openCards[1].firstElementChild
           .className) {
           openCards[0].classList.add('match');
           openCards[1].classList.add('match');
           cardCounter = 0;
-          /** All cards matched/game end. */
-          if (document.getElementsByClassName('match').length === 16) {
-            /** Stop the timer. */
-            clearInterval(intervalId);
-            intervalId = null;
-            showResultPage();
-          }
-          /** Two cards opened and do not match. */
+          // The cards do not match
         } else {
           setTimeout(function closeCards() {
             openCards.forEach(function(card) {
@@ -190,8 +189,15 @@ document.getElementsByClassName('deck')[0].addEventListener('click',
             cardCounter = 0;
           }, 900);
         }
-        /** Update score after every move. */
+        // Update score after every move
         updateScore();
+        // All cards matched/game end
+        if (document.getElementsByClassName('match').length === 16) {
+          // Stopping the timer
+          clearInterval(intervalId);
+          intervalId = null;
+          showResultPage();
+        }
       }
     }
   });
@@ -202,7 +208,8 @@ document.getElementsByClassName('deck')[0].addEventListener('click',
 document.getElementsByClassName('restart')[0].addEventListener('click', init);
 
 /**
- * Update score (moves and stars) after one move and set the star counter.
+ * Update score (moves and stars) and setting of the star counter
+ * after every move.
  */
 function updateScore() {
   moveCounter += 1;
@@ -233,6 +240,7 @@ function setStarEmpty(node) {
  * Show result page (and hide game part).
  */
 function showResultPage() {
+  // Update the score data
   document.getElementsByClassName('movesTotal')[0].textContent = moveCounter;
   document.getElementsByClassName('starCounter')[0].textContent = starCounter;
   if (starCounter === 1) {
@@ -242,8 +250,9 @@ function showResultPage() {
   }
   document.getElementsByClassName('time')[0].textContent =
     document.getElementById('realtime').textContent;
-  /** Event listener definition for 'Play again' button. */
+  // Event listener definition for 'Play again' button
   document.getElementById('play-btn').addEventListener('click', init);
+  // Hide the game and show the result page
   document.querySelector('header').classList.add("hide");
   document.querySelector('.score-panel').classList.add("hide");
   document.querySelector('.deck').classList.add("hide");
